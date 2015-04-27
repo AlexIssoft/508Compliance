789deba1c2ff8863cc85df18174ab43b8606b717
Michael Mikhnovets <michnovec@tut.by>
2015-04-27 18:46:30 +0300

    [+] repository interfaces were added

---
+++ Core/Interfaces/ILessonRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Core.Models;
+using Core.Repositories.Interfaces;
+
+namespace Core.Interfaces
+{
+    public interface ILessonRepository : IRepository
+    {
+        LessonModel GetLesson(int lessonId);
+        List<LessonModel> GetAllLessons();
+        void AddLesson(LessonModel lesson);
+        void UpdateLesson(LessonModel lesson);
+        void RemoveLesson(int lessonId);
+    }
+}
---
+++ Core/Interfaces/INoteRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Core.Models;
+
+namespace Core.Interfaces
+{
+    public interface INoteRepository
+    {
+        NoteModel GetNote(int noteId);
+        List<NoteModel> GetAllNotes();
+        void AddNote(NoteModel note);
+        void UpdateNote(NoteModel note);
+        void RemoveNote(int noteId);
+    }
+}
